8770d641a3cb13bb7e8022987e09131aa4be2b31
Aftermatheus <158469851+Aftermatheus@users.noreply.github.com>
2024-02-05 12:49:44 -0300

    Update fadvet.js

--- src/js/fadvet.js
+++ src/js/fadvet.js
@@ -1,14 +1,28 @@
 let selectedSinais = []; // Lista para armazenar os sinais clínicos selecionados
+let isTyping = false; // Variável para controlar se o usuário está digitando
+let selectedSinalClicked = false; // Variável para controlar se um sinal foi selecionado e a barra de pesquisa foi clicada
+
+// Event listener para fechar a lista de resultados quando clicar fora dela
+document.addEventListener('click', function(event) {
+    const listaResultados = document.getElementById('lista-de-resultados');
+    const searchBar = document.getElementById('searchbar');
+
+    if (!listaResultados.contains(event.target) && event.target !== searchBar) {
+        listaResultados.style.display = 'none';
+    }
+});
 
 function search() {
     let input = removeAccents(document.getElementById('searchbar').value.toLowerCase());
     let x = document.getElementsByClassName('sinais');
 
-    if (input === '') {
+    if (input === '' && !isTyping && !selectedSinalClicked) {
+        document.getElementById('lista-de-resultados').style.display = 'none';
         for (let i = 0; i < x.length; i++) {
             x[i].style.display = "none";
         }
     } else {
+        document.getElementById('lista-de-resultados').style.display = 'block';
         for (let i = 0; i < x.length; i++) {
             let textWithoutAccents = removeAccents(x[i].innerHTML.toLowerCase());
             if (!textWithoutAccents.includes(input)) {
@@ -18,11 +32,13 @@ function search() {
             }
         }
     }
+
+    selectedSinalClicked = false;
 }
 
 function removeAccents(text) {
     return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-}
+}   
 
 // Função para lidar com a seleção de sinais clínicos
 function selectSinal(sinal) {
@@ -33,27 +49,26 @@ function selectSinal(sinal) {
         selectedSinais.splice(index, 1); // Remove se estiver presente
     }
 
-    // Limpar barra de pesquisa
     document.getElementById('searchbar').value = '';
-
-    // Ocultar área de resultados
+    
     document.getElementById('lista-de-resultados').style.display = 'none';
-
-    // Resetar lista de resultados
+    
     let x = document.getElementsByClassName('sinais');
     for (let i = 0; i < x.length; i++) {
         x[i].style.display = "list-item";
     }
-
+    
     displaySelectedSinais();
+
+    selectedSinalClicked = true;
 }
 
 // Função para exibir os sinais clínicos selecionados em caixas com botão de exclusão
 function displaySelectedSinais() {
     let selectedSinaisContainer = document.getElementById('selected-sinais');
-    selectedSinaisContainer.innerHTML = ''; // Limpa o conteúdo atual
+    selectedSinaisContainer.innerHTML = ''; 
 
-    selectedSinais.forEach(function (sinal) {
+    selectedSinais.forEach(function(sinal) {
         let sinalBox = document.createElement('div');
         sinalBox.classList.add('selected-sinal-box');
 
@@ -64,7 +79,7 @@ function displaySelectedSinais() {
         let closeButton = document.createElement('button');
         closeButton.textContent = 'x';
         closeButton.classList.add('close-button');
-        closeButton.addEventListener('click', function () {
+        closeButton.addEventListener('click', function() {
             removeSelectedSinal(sinal);
         });
         sinalBox.appendChild(closeButton);
@@ -78,6 +93,19 @@ function removeSelectedSinal(sinal) {
     let index = selectedSinais.indexOf(sinal);
     if (index !== -1) {
         selectedSinais.splice(index, 1);
-        displaySelectedSinais(); // Atualiza a exibição dos sinais selecionados
+        displaySelectedSinais(); 
     }
 }
+
+// Event listener para controlar se o usuário está digitando
+document.getElementById('searchbar').addEventListener('input', function() {
+    isTyping = true;
+    if (document.getElementById('searchbar').value === '') {
+        document.getElementById('lista-de-resultados').style.display = 'none';
+    }
+});
+
+// Event listener para controlar quando o usuário parou de digitar
+document.getElementById('searchbar').addEventListener('blur', function() {
+    isTyping = false;
+});
